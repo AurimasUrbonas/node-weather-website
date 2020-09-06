c4103a4e02eeb2dead5b738371a3adc03d8107ce
Aurimas Urbonas <aurbonas13@gmail.com>
2020-09-06 14:08:11 +0300

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
 const publicDirPath = path.join(__dirname, '../public');
@@ -98,7 +99,6 @@ app.get('*', (req, res) => {
 });
 
 // Run server on port
-app.listen(3000, () => {
-  console.log('Server is running on port 3000.');
-  console.log('http://localhost:3000');
+app.listen(port, () => {
+  console.log('Server is running on port ' + port + '.');
 });
